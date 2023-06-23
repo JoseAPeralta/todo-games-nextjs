e069718058be406add7dd5062bb395b3ccb1e14c
Jose A Peralta <japc1802@hotmail.com>
2023-06-23 18:14:55 -0500

    feat: :sparkles: add footer

---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="flex font-bold uppercase text-slate-200 hover:no-underline">
+      <div className="text-5xl pr-1 border-r-2 border-slate-200">TG</div>
+      <div className="pl-2">
+        <div>t o t a l</div>
+        <div>g a m e s</div>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
