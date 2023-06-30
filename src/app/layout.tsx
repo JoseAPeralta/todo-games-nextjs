ef2dddcd87df029880cbeec556da07efa852b948
Jose A Peralta <japc1802@hotmail.com>
2023-06-30 13:21:42 -0500

    feat: :sparkles: add next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import "./globals.css";
 import "@/styles/main.scss";
 import { Navbar, Footer } from "@/app/components";
 import { Lato } from "next/font/google";
-
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 const font_primary = Lato({
   subsets: ["latin"],
   variable: "--font-primary",
@@ -16,15 +17,17 @@ export const metadata = {
   description: "Video games information",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en" className={`${font_primary.variable} `}>
       <body>
         <Navbar />
+        <pre>{JSON.stringify(session)}</pre>
         <main className="container-fluid 2xl:container">{children}</main>
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 import Genres from "@/app/genres/page";
+import {
+  LoginButton,
+  LogoutButton,
+  ProfileButton,
+  RegisterButton,
+} from "@/app/components/buttons.component";
+
 export default function Home() {
   return (
     <main>
+      <div>
+        <LoginButton />
+        <RegisterButton />
+        <LogoutButton />
+        <ProfileButton />
+      </div>
       <Genres />
     </main>
   );
